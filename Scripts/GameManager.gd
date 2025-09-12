2e606face40d339a4573a7194391ac56710646e8
Mike Murray <mikemurray056@gmail.com>
2025-09-12 10:43:31 -0400

    Can now hover over cards to get data
    Need to find a way to prevent player from hovering over the deck to see the cards in there.

--- Scripts/GameManager.gd
+++ Scripts/GameManager.gd
@@ -2,10 +2,10 @@ extends Node3D
 class_name GameManager
 
 
-signal update_card_details(text: String)
+signal update_card_details(show_window: bool, text: String)
 
 @onready var camera = $Camera3D
-var poll_rate: float = 0.15				#used to prevent game from checking for mouse input every frame
+var poll_rate: float = 0.3				#used to prevent game from checking for mouse input every frame
 var last_poll_time: float
 
 #player data
@@ -35,34 +35,38 @@ func _process(delta: float) -> void:
 	#_get_mouse_input()
 	pass
 
+##Show card's information if the mouse hovers over it for a duration.
 func _get_mouse_input():
 	var time = Time.get_unix_time_from_system()
 	if time < last_poll_time + poll_rate:
 		return
 		
 	last_poll_time = time
-	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+	#if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
 		#cast a ray
-		var ray_dist: float = 1000
-		var space_state = get_world_3d().direct_space_state
-		var origin = camera.project_ray_origin(camera.get_viewport().get_mouse_position())
-		var hit = origin + camera.project_ray_normal(camera.get_viewport().get_mouse_position()) * ray_dist
-		#if a card was hit, display its information
-		var query = PhysicsRayQueryParameters3D.create(origin, hit)
-		query.collide_with_areas = true
+	var ray_dist: float = 1000
+	var space_state = get_world_3d().direct_space_state
+	var origin = camera.project_ray_origin(camera.get_viewport().get_mouse_position())
+	var hit = origin + camera.project_ray_normal(camera.get_viewport().get_mouse_position()) * ray_dist
+	#if a card was hit, display its information
+	var query = PhysicsRayQueryParameters3D.create(origin, hit)
+	query.collide_with_areas = true
 
-		var result:Dictionary = space_state.intersect_ray(query)	#this collects all information from the ray cast and places them in dictionary
-		#print(result)
-		
-		#display card info.
-		if result.has("collider"):
-			var card: Card = result["collider"]
-			if card != null and card is Card:
-				var card_type = CardData.CardType.keys()[card.card_type]	#prints the name of the enum value
-				var value = card.card_value
-				print("Card is {0} with value {1} ".format([card_type, value]))
-				var card_text = "{0}\nValue: {1}".format([card_type, value])
-				update_card_details.emit(card_text)							#HUD will be updated
+	var result:Dictionary = space_state.intersect_ray(query)	#this collects all information from the ray cast and places them in dictionary
+	#print(result)
+	
+	#display card info.
+	if result.has("collider"):
+		var card: Card = result["collider"]
+		if card != null and card is Card:
+			var card_type = CardData.CardType.keys()[card.card_type]	#prints the name of the enum value
+			var value = card.card_value
+			print("Card is {0} with value {1} ".format([card_type, value]))
+			var card_text = "{0}\nValue: {1}".format([card_type, value])
+			update_card_details.emit(true, card_text)							#HUD will be updated			
+	else:
+		update_card_details.emit(false ,"")
+
 
 func _physics_process(delta: float) -> void:
 	_get_mouse_input()
--- Scripts/hud.gd
+++ Scripts/hud.gd
@@ -1,13 +1,14 @@
 extends Control
 class_name HUD
 
-@onready var card_text = $"Card Details/Card Text Background/Card Text"
-@onready var card_container = $"Card Details"
+@onready var card_text = $"Card HUD/Card Text"
+@onready var card_container = $"Card HUD"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	Singleton.hud = self
 	
+	card_container.visible = false
 	#get signal from game manager
 	var game_manager = $".."
 	game_manager.update_card_details.connect(show_card_details)
@@ -16,5 +17,6 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
-func show_card_details(text: String):
+func show_card_details(show_window: bool, text: String):
+	card_container.visible = show_window
 	card_text.text = text
