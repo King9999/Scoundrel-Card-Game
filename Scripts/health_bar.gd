ee0b9f57d3446d3dafdaabbc1dc8e46449cd2247
Mike Murray <mikemurray056@gmail.com>
2025-09-29 10:59:03 -0400

    Added code to flash health bar
    health bar will flash during damage preview.

--- Scripts/health_bar.gd
+++ Scripts/health_bar.gd
@@ -10,34 +10,44 @@ class_name HealthBar
 @onready var healthbar_progress: Sprite2D = $Progress
 @onready var healthbar_fill: Sprite2D = $Fill
 var max_health: float					#this value is based on the health bar's X scale value
+var flash_bar: bool						#if true, progress bar flashes
+var last_flash_time: float
+var flash_rate: float = 0.25
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	max_health = healthbar_fill.scale.x		
-	print("bar width: " + str(max_health))
-	pass # Replace with function body.
+	#print("bar width: " + str(max_health))
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	#var new_x = healthbar_fill.scale.x - 0.1 * delta
 	#healthbar_fill.scale.x = new_x
-	pass
+	if (flash_bar == true):
+		var time = Time.get_unix_time_from_system()
+		if time > last_flash_time + flash_rate:
+			last_flash_time = time
+			healthbar_progress.visible = !healthbar_progress.visible
+				
 
-##Shows how much damage player will take before taking the action
+##Shows how much damage player will take before taking the action. [br]
+##[param damage_amount] is the damage the player will take, out of 20. This value is converted to a percentage, which is reflected in the bar.
 func show_damage_preview(damage_amount: float):
 	var game_manager = Singleton.game_manager
 	var damage_percent = (game_manager.hit_points - damage_amount) / game_manager.max_hit_points
 	#shows how much damage the player will take if they attack a monster
 	#amount is the percentage of bar that will be lost.
-	#the damage portion flashes to get player attention.
+	
 	damage_percent = 1 if damage_percent > 1 else damage_percent
 	damage_percent = 0 if damage_percent < 0 else damage_percent
 	#print("damage %: " + str(1 - damage_percent))
 	healthbar_fill.scale.x *= damage_percent
-	pass
+	#the damage portion flashes to get player attention.
+	flash_bar = true
 
-func reduce_healthbar(amount: float):
+##Reduces health bar by converting [param damage_amount] into a percentage amount.
+func reduce_healthbar(damage_amount: float):
 	#health bar is reduced by percentage amount
 	#fill bar is reduced immediately, while progress bar slowly decreases
 	pass
